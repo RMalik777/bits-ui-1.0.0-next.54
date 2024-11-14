c4d97bd5975681bb77b495439fc2094394a3cf4c
RMalik777 <Rafli.Malik@gmail.com>
2024-11-14 14:39:22 +0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+
+	/** @type {{children: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
+<main class="w-dvw h-dvh">
+	{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import * as Select from "$lib/components/ui/select/index.js"
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<Select.Root type="single">
+  <Select.Trigger>Open</Select.Trigger>
+  <Select.Content>
+    <Select.Item value="light">Light</Select.Item>
+    <Select.Item value="dark">Dark</Select.Item>
+    <Select.Item value="system">System</Select.Item>
+  </Select.Content>
+</Select.Root>
+
+
