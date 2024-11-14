54835d7cd403893b9b278d5b31f95c997e858649
Rafli Malik <Rafli.Malik@gmail.com>
2024-11-14 15:08:14 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import * as Select from "$lib/components/ui/select/index.js"
-	let value = $state()
+	let value = $state("System")
 </script>
 
 <svelte:head>
@@ -8,11 +8,11 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<Select.Root type="single" value={value}>
+<Select.Root type="single" bind:value={value}>
   <Select.Trigger>{value}</Select.Trigger>
   <Select.Content>
-    <Select.Item value="light">Light</Select.Item>
-    <Select.Item value="dark">Dark</Select.Item>
-    <Select.Item value="system">System</Select.Item>
+    <Select.Item value="Light">Light</Select.Item>
+    <Select.Item value="Dark">Dark</Select.Item>
+    <Select.Item value="System">System</Select.Item>
   </Select.Content>
 </Select.Root>
